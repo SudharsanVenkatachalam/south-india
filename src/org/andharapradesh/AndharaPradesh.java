a6579510cd708f32f28805514cda535ec951e036
sudharsan <sudharsanworklife@gmail.com>
2024-10-18 10:42:49 +0530

    code added

---
+++ src/org/andharapradesh/AndharaPradesh.java
@@ -0,0 +1,8 @@
+package org.andharapradesh;
+
+public class AndharaPradesh {
+public void telugu() {
+	// TODO Auto-generated method stub
+System.out.println("pawan");
+}
+}
---
+++ src/org/kerala/Kerala.java
@@ -0,0 +1,10 @@
+package org.kerala;
+
+import org.andharapradesh.AndharaPradesh;
+
+public class Kerala extends AndharaPradesh {
+public void malayalam() {
+	// TODO Auto-generated method stub
+System.out.println("chetta");
+}
+}
